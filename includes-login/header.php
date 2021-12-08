c0e4e847f94aea77b7656ef9f7c20e5c2ef12652
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-08 09:52:35 +0100

    Create header.php

---
+++ includes-login/header.php
@@ -0,0 +1,11 @@
+<?php require_once("connexio_mysqli.php");?>
+
+<!doctype html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+         <link rel="stylesheet" href="../bootstrap/bootstrap.min.css">
+         <link rel="stylesheet" href="css-login/style-login.css">
+         <title><?php echo $title;?></title>
+   </head>
