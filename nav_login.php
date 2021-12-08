a46e40ccd78e010d3019614cc6b4935db700d54e
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-08 09:59:21 +0100

    Create nav_login.php

---
+++ nav_login.php
@@ -0,0 +1,14 @@
+   <div id="myNav" class="overlay d-flex">
+
+      <div class="overlay-content align-self-start p-3 text-center">
+         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a onclick="closeNav()" href="https://xavideveloper.com">Inici</a>
+            <a onclick="closeNav()" href="index-login.php">Accés</a>
+            <a onclick="closeNav()" href="registre.php">Registre</a>
+            <a onclick="closeNav()" href="../index-projects.php">Projectes</a>
+      </div>
+   </div>
+
+   <div class="container-burguer position-fixed">
+      <img class="icon-burguer" onclick="openNav()" src="img/icon-burguer.svg" alt="icon-menu">
+   </div>
