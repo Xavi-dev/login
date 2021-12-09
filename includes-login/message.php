26ae0019f8ccb81402ab44f8db08d445fe90bf79
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-09 08:45:51 +0100

    Delete message.php

--- includes-login/message.php
+++
@@ -1,6 +0,0 @@
-<?php if(isset($registrationOk)) : ?>
-      <div class="alert alert-dismissible fade show message bg-green-color" role="alert">
-         <span><?php echo $registrationOk; ?></span> 
-         <button type="button" class="btn-close" id="x-alert" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-   <?php endif; ?>
