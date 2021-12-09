5968d374f12370d3c396379e53f27d454ed5425d
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-09 08:37:29 +0100

    Update connection_mysqli.php

--- connection_mysqli.php
+++ connection_mysqli.php
@@ -8,7 +8,7 @@
     $conn = new mysqli($server,$user,$pass,$dbname);
 
     if(mysqli_connect_errno()){
-        echo "Error. No connectat a la base de dades", mysqli_connect_error();
+        echo "Error. Not connected to database.", mysqli_connect_error();
         exit();
     }
 
