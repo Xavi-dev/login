8f324020edfb139bdd39327906c6264c204b8caa
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-09 08:24:53 +0100

    Update login_session_accepted.php

--- login_session_accepted.php
+++ login_session_accepted.php
@@ -1,13 +1,13 @@
 <?php
 
-$title = "Sessió iniciada | xavideveloper";
-require_once("connexio_mysqli.php");
+$title = "Logged in";
+require_once("connection_mysqli.php");
 require_once("includes-login/header.php"); 
 
 session_start();
 
    if(!isset($_SESSION['id'])) {
-      header("Location: index-login.php");
+      header("Location: index_login.php");
    }
 
    $iduser = $_SESSION['id']; 
@@ -27,7 +27,7 @@ session_start();
       <div class="flex-sm-row align-self-center">
 
          <div>
-            <h1 class="text-grey"><?php echo utf8_decode($row['user']) . " ha iniciat sessió."; ?></h1>
+            <h1 class="text-grey"><?php echo utf8_decode($row['user']) . " is logged in."; ?></h1>
          </div>
             
          <div class="justify-content-center mb-2">
@@ -35,7 +35,7 @@ session_start();
                <a href="exit_session.php"><img src="img/icon_session_close.svg" class="icon-close-session mt-3 mb-2" alt=""></a>
             </div>
 
-         <p class="text-center"><a href="exit_session.php" class="text-grey">Tancar sessió</a></p>
+         <p class="text-center"><a href="exit_session.php" class="text-grey">Log out</a></p>
   
          </div>
       </div>
