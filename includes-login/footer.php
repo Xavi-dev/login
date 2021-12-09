25ac007908ffd2eb7600c75a9eadf0bd4e463a83
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-09 08:51:23 +0100

    Update footer.php

--- includes-login/footer.php
+++ includes-login/footer.php
@@ -1,4 +1,4 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="../bootstrap/bootstrap.min.js"></script>
 <script src="../bootstrap/bootstrap.bundle.min.js"></script>
-<script src="../js/js_xtheme.js"></script>
+<script src="../js/main.js"></script>
